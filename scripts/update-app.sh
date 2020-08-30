fc6bb6b1d666261649154e99c52b7f802333e72e
Nick Martinez <nick@xdmtk.org>
2020-08-29 19:19:25 -0700

    #5 - Add basic pull script for system cron task

---
+++ scripts/update-app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+OA_BRANCH=$1
+if [ $OA_BRANCH="develop" ]; then
+    CD_LOCATION="/home/dev/oa"
+elif [ $OA_BRANCH="master" ]; then
+    CD_LOCATION="/var/www/oa"
+else
+    echo "ERROR: Invalid branch specified"
+    exit
+fi
+
+cd $CD_LOCATION
+git reset --hard
+git clean -fd
+git checkout $OA_BRANCH
+git pull
