38b13aef168c78be3df5703e4ad68bfe073ff6af
Nick Martinez <nick@xdmtk.org>
2020-09-07 01:27:59 -0700

    #2 - Save message and users impl for later

--- resources/views/admin/components/navigation.blade.php
+++ resources/views/admin/components/navigation.blade.php
@@ -3,8 +3,10 @@
         <li class="dashboard"><a href="/admin">Dashboard</a></li>
         @if(Auth::user() && Auth::user()->admin)
             <li class="write"><a href="/admin?page=logs">Logs</a></li>
+        <!-- Implement these later
             <li class="comments"><a href="#">Messages</a></li>
             <li class="users"><a href="#">Users</a></li>
+        -->
         @endif
     </ul>
 </nav>
