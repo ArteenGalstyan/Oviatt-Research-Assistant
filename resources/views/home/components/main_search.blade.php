c6a172cf1f8d00892dd4a4901b9286acd34e9f7a
Nick Martinez <nick@xdmtk.org>
2020-10-14 20:26:43 -0700

    #16 - Add powered by logo

--- resources/views/home/components/main_search.blade.php
+++ resources/views/home/components/main_search.blade.php
@@ -1,11 +1,17 @@
-<body>
+<body xmlns="http://www.w3.org/1999/html">
 <div class="wrapper">
+
+    <!-- Logo + Particles -->
     <script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/tsparticles@1.17.0-beta.7/dist/tsparticles.min.js"></script>
     <div id="particles" class="half-background">
         <img src="{{asset('img/mainlogo.png')}}">
     </div>
+
+
     <div class="container">
+
+        <!-- Search bar -->
         <form role="search" method="get" class="search-form form" action="">
             <label>
                 <span class="screen-reader-text">Search for...</span>
@@ -13,10 +19,13 @@
             </label>
             <input type="submit" class="search-submit button" value="&#xf002" />
         </form>
+
+        <!-- Optional info bar - Can't make this fit on mobile -->
         @if (!$isMobile)
             <div id="about-home">
-                Test
+                <span id="about-home-text">Powered by Machine Learning</span>
             </div>
+            <img id="powered-by" src="{{asset('img/aibrain.png')}}">
         @endif
     </div>
 </div>
