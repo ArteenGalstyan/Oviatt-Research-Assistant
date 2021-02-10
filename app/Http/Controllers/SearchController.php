e8b1835fddc73b484948a45aeb1a7a759ce34099
Nick Martinez <nick@xdmtk.org>
2021-02-10 07:32:41 -0800

    #30 - Connect search card generation to API

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,6 +10,57 @@ use Illuminate\Support\Facades\DB;
 class SearchController extends Controller {
 
     public function search()  {
-        return view('search.layout');
+        return view('search.layout', [
+            'results' => $this->get_search_results(
+                Request::get('s')
+            )
+        ]);
+    }
+
+    private function get_search_results($query) {
+        return [
+            [
+                'image' => asset('img/csun-icon.png'),
+                'title' => 'Example Title',
+                'description' => 'Example description',
+                'source' => 'https://google.com',
+                'source_title' => 'Google'
+            ],
+            [
+                'image' => asset('img/csun-icon.png'),
+                'title' => 'Example Title',
+                'description' => 'Example description',
+                'source' => 'https://google.com',
+                'source_title' => 'Google'
+            ],
+            [
+                'image' => asset('img/csun-icon.png'),
+                'title' => 'Example Title',
+                'description' => 'Example description',
+                'source' => 'https://google.com',
+                'source_title' => 'Google'
+            ],
+            [
+                'image' => asset('img/csun-icon.png'),
+                'title' => 'Example Title',
+                'description' => 'Example description',
+                'source' => 'https://google.com',
+                'source_title' => 'Google'
+            ],
+            [
+                'image' => asset('img/csun-icon.png'),
+                'title' => 'Example Title',
+                'description' => 'Example description',
+                'source' => 'https://google.com',
+                'source_title' => 'Google'
+            ],
+            [
+                'image' => asset('img/csun-icon.png'),
+                'title' => 'Example Title',
+                'description' => 'Example description',
+                'source' => 'https://google.com',
+                'source_title' => 'Google'
+            ],
+        ];
     }
 }
